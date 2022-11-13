3d8d121962f42725187dd1d1b9b1f163586c1afc
daniel le <daniel97.le@gmail.com>
2022-11-13 14:30:21 -0700

    commit

--- first-fullstack.client/vite.config.js
+++ first-fullstack.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'docs',
+    outDir: '../first-fullstack/client',
     sourcemap: false
   },
   server: {
